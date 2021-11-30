a0b3a388d60fb29a6a8ee92fe03870f539df45eb
ptcheng0812 <ptcheng0812@outlook.com>
2021-11-30 17:41:59 +0800

    profile and user layout css

--- src/pages/my/profile.jsx
+++ src/pages/my/profile.jsx
@@ -1,8 +1,27 @@
+import Link from 'next/link'
+import Avatar from '@mui/material/Avatar'
+import Grid from '@mui/material/Grid'
+import CardContent from '@mui/material/CardContent'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+
 import { useState } from 'react'
 import useUser from '@/_hooks/user'
 
 import CompsLayout from '@/components/layouts/Layout'
 import CompsModalsMyProfileUpdate from '@/components/modals/my-profile/update'
+
+const ColoredLine = ({ color }) => (
+  <hr
+    style={{
+      color,
+      backgroundColor: color,
+      height: 2
+    }}
+    className="coloured-line"
+
+  />
+)
 
 function PagesMyProfile() {
   const { currentUser, myProfileUpdate } = useUser()
@@ -12,22 +31,43 @@ function PagesMyProfile() {
 
   return (
     <CompsLayout>
-      <div id="pages-private">
+      <div id="pages-private" className="d-flex flex-column">
         <h1>Private Page</h1>
-        <h2>{currentUser?.email}</h2>
-        <h2>{currentUser?.name}</h2>
-        <h2>{currentUser?.firstName}</h2>
-        <h2>{currentUser?.lastName}</h2>
-        <h2>{currentUser?.dateOfBirth}</h2>
-        <h2>{currentUser?.introduction}</h2>
-        <img src={currentUser?.avatar} alt="avatar" />
-        <button
-          className="btn btn-info btn-sm"
-          type="button"
-          onClick={() => {
-            setOpenMyProfileEdit(true)
-          }}
-        >Edit Your Profile</button>
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} id="page-my-profile-main-grid">
+          <Grid item xs={4} id="pages-my-profile-avatar-grid" className="d-flex justify-content-center border rounded">
+            <Avatar src={currentUser?.avatar} alt="avatar" sx={{ width: 200, height: 200 }} id="pages-my-profile-avatar" />
+          </Grid>
+          <Grid item xs={6} id="pages-my-profile-info-grid">
+            <h6>Email: {currentUser?.email}</h6>
+            <ColoredLine color="black" />
+            <h6>Name: {currentUser?.name}</h6>
+            <h6>Full Name: {currentUser?.firstName} {currentUser?.lastName}</h6>
+            <h6>Date of Birth: {currentUser?.dateOfBirth}</h6>
+            <ColoredLine color="black" />
+            <Typography variant="subtitle2" gutterBottom component="div">
+              Introduction:
+            </Typography>
+            <h6>{currentUser?.introduction}</h6>
+            <Button
+              className="btn btn-info btn-sm"
+              id="edit-profile-btn"
+              type="button"
+              onClick={() => {
+                setOpenMyProfileEdit(true)
+              }}
+            >Edit Your Profile</Button>
+          </Grid>
+          <Grid item xs={4} spacing={2} className="in-pages-nav border rounded">
+            <CardContent className="d-flex flex-column">
+              <Link href="/my/profile">Profile</Link>
+              <Link href="/my/threads">My Threads</Link>
+              <Link href="/my/posts">My Posts</Link>
+              <Link href="/my/notifications">My Notifications</Link>
+              <Link href="/my/favourites">My Favourites</Link>
+            </CardContent>
+          </Grid>
+          <Grid item xs={6} />
+        </Grid>
       </div>
 
       <CompsModalsMyProfileUpdate
--- src/pages/users/[userId].jsx
+++ src/pages/users/[userId].jsx
@@ -1,3 +1,10 @@
+import Link from 'next/link'
+import Avatar from '@mui/material/Avatar'
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
+import CardContent from '@mui/material/CardContent'
+import Button from '@mui/material/Button'
+
 import { useState } from 'react'
 import { useRouter } from 'next/router'
 
@@ -5,6 +12,17 @@ import CompsLayout from '@/components/layouts/Layout'
 import CompsModalsNotificationsCreate from '@/components/modals/notifications/create'
 import usePublicUser from '@/_hooks/publicUser'
 import useNotifications from '@/_hooks/notifications'
+
+const ColoredLine = ({ color }) => (
+  <hr
+    style={{
+      color,
+      backgroundColor: color,
+      height: 2
+    }}
+    className="coloured-line"
+  />
+)
 
 function PagesPublicUser() {
   const [openNotificationsCreate, setOpenNotificationsCreate] = useState(false)
@@ -19,20 +37,44 @@ function PagesPublicUser() {
     <CompsLayout>
       <div id="pages-public-user">
         <h1>User Page</h1>
-        <div id="public-user-info">
-          <h4>{user?.name}</h4>
-          <h4>{user?.email}</h4>
-          <h4>{user?.firstName} {user?.lastName}</h4>
-          <h4>{user?.dateOfBirth}</h4>
-          <h4>{user?.introduction}</h4>
-          <button
-            className="btn btn-primary btn-sm"
-            type="button"
-            onClick={() => {
-              setOpenNotificationsCreate(true)
-            }}
-          >Message</button>
-        </div>
+        <Grid container spacing={2} rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} id="pages-user-main-grid">
+          <Grid item xs={4} id="pages-user-avatar-grid" className="border rounded">
+            <div>
+              <Avatar src={user?.avatar} alt="avatar" sx={{ width: 200, height: 200 }} id="pages-user-avatar" />
+              <div className="d-flex justify-content-center" id="pages-user-message-btn">
+                <Button
+                  className="btn btn-primary btn-sm"
+                  type="button"
+                  onClick={() => {
+                    setOpenNotificationsCreate(true)
+                  }}
+                >Message</Button>
+              </div>
+            </div>
+          </Grid>
+          <Grid item xs={6} id="pages-user-info-grid">
+            <h6>Email: {user?.email}</h6>
+            <ColoredLine color="black" />
+            <h6>Name: {user?.name}</h6>
+            <h6>Full Name: {user?.firstName} {user?.lastName}</h6>
+            <h6>Date of Birth: {user?.dateOfBirth}</h6>
+            <ColoredLine color="black" />
+            <Typography variant="subtitle2" gutterBottom component="div">
+              Introduction:
+            </Typography>
+            <h6>{user?.introduction}</h6>
+          </Grid>
+          <Grid item xs={4} spacing={2} className="in-pages-nav border rounded">
+            <CardContent className="d-flex flex-column">
+              <Link href="/my/profile">Profile</Link>
+              <Link href="/my/threads">My Threads</Link>
+              <Link href="/my/posts">My Posts</Link>
+              <Link href="/my/notifications">My Notifications</Link>
+              <Link href="/my/favourites">My Favourites</Link>
+            </CardContent>
+          </Grid>
+          <Grid item xs={6} />
+        </Grid>
       </div>
 
       <CompsModalsNotificationsCreate
